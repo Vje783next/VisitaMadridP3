a6799a10dc4590aa3b322c08483be35ae6399434
Vje <vje783next@gmail.com>
2025-01-17 17:17:16 +0100

    commit 7 critical index

--- critical.js
+++ critical.js
@@ -1,13 +1,27 @@
-import {generate} from 'critical';
+import { generate } from 'critical';
+
+console.log('Starting critical CSS generation...');
+
 
 generate({
-  inline: false,
+  inline: true,
   base: 'src/',
-  src: 'index.html',
+  src: 'http://127.0.0.1:8080/index.html',
   target: {
+    html: 'index_critico.html',
     css: 'critical.css',
-    html: 'index-critical.html'
-  },  
-  width: 1300,
-  height: 900,
+  },  dimensions: [
+    {
+      height: 500,
+      width: 300,
+    },
+    {
+      height: 720,
+      width: 1280,
+    },
+  ],
+}).then(() => {
+  console.log('Generated critical CSS');
+}).catch((err) => {
+  console.error(err);
 });
